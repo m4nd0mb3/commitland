774080396945753473b5f320a854d4599bcee257
jonathan.mandombe <jonathan.mandombe@ideiasdinamicas.com>
2022-09-09 08:48:02 +0100

    BackEnd environment

--- commitland/src/extension.ts
+++ commitland/src/extension.ts
@@ -13,14 +13,41 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commitland.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('commitland.commitLand', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code from CommitLand!');
+		vscode.window.showInformationMessage('Welcome to CommitLand!');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('commitland.start', () => {
+		// Create and show panel
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding',
+			'Cat Coding',
+			vscode.ViewColumn.One,
+			{}
+		);
+
+		// And set its HTML content
+		panel.webview.html = getWebviewContent();
+	}));
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
