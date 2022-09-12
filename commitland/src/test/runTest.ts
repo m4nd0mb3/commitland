01f07d93fc417b6965450342e7c576f7139935ee
ManuelismoXp <lukau.garcia@ideiasdinamicas.com>
2022-09-12 09:50:34 +0100

    Testando conexão/permissão de escrita no git/repo

--- commitland/src/test/runTest.ts
+++ commitland/src/test/runTest.ts
@@ -15,8 +15,10 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+
 		console.error('Failed to run tests');
 		process.exit(1);
+		
 	}
 }
 
